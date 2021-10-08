8243f6b11f5c4420462ca89adc02e793ddb35b10
Khai <khai.ngo@stfc.ac.uk>
2021-10-08 15:45:07 +0100

    Fix bugs - runs properly now

--- writeTimeTrace.py
+++ writeTimeTrace.py
@@ -1,5 +1,4 @@
 import sys
-import matplotlib.pyplot as plt
 def readTime(inputPath):
     with open(inputPath) as f:
         t = []
@@ -8,27 +7,28 @@ def readTime(inputPath):
             t.append(line.split()[2])
         return t
 def writeTimeTrace (timeArr, filename):
-    currentCycle = 1.0
-    cycle = 1.0
+    currentCycle = 0
+    target = 1.0
     count = 0
-    f = open(filename, "w")
+    f = open("{}.txt".format(filename), "w")
     f.write("Time (s)\tCount\n")
     for t in timeArr:
-        if t > currentCycle:
+        if t > currentCycle +  target:
             f.write("{}\t{}\n".format(currentCycle, count))
-            count = 0
-            currentCycle += cycle
+            count = 1
+            currentCycle += target
         else: 
             count +=1
-    print("Output time trace to {}".format(filename))
+    print("Output time trace to {}.txt".format(filename))
+    f.close()
 if __name__ == '__main__':
     t = readTime(sys.argv[1])
-    FirstTimeC = t[0]
+    FirstTimeC = float(t[0])
     time_elasped_microsec = 0
     Reset = 0
     t_elasped_sec = []
     for timeStamp in t:
-        currentTimeStamp = timeStamp + Reset - FirstTimeC
+        currentTimeStamp = float(timeStamp) + Reset - FirstTimeC
         if currentTimeStamp < time_elasped_microsec:
             Reset = Reset + 17179869.19
             time_elasped_microsec = currentTimeStamp + 17179869.19
@@ -36,4 +36,6 @@ if __name__ == '__main__':
             time_elasped_microsec = currentTimeStamp
         t_elasped_sec.append(time_elasped_microsec / 1000000.0)
     writeTimeTrace(t_elasped_sec, sys.argv[2])
+    t.clear()
+    t_elasped_sec.clear()
     
