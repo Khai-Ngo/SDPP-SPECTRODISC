5e0d71303aaf36ea8b451370e1c5dc8bc7818eb0
Khai <khai.ngo@stfc.ac.uk>
2021-10-08 11:43:37 +0100

    Delete redundant methods

--- plotPHS.py
+++ plotPHS.py
@@ -20,17 +20,6 @@ def twoDdataSelector (x, y, xlow = 0, xhigh = np.inf, ylow = 0, yhigh = np.inf, 
     xret = np.array(xret)
     yret = np.array(yret)
     return xret, yret
-def oneDdataSelector (x, xlow = 0, xhigh = np.inf, numPoint = np.inf):
-    xret = []
-    count = 0
-    for i in range(len(x)):
-        if x[i] <= xhigh and x[i] >= xlow:
-            xret.append(x[i])
-            count += 1
-        if count > numPoint:
-            break
-    xret = np.array(xret)
-    return xret 
 # write PAS to file
 def buildHist(arr, filename, minVal, maxVal, noOfBins, auto = False, plot = False, save = True):
     print("Building histogram...")
@@ -48,33 +37,6 @@ def buildHist(arr, filename, minVal, maxVal, noOfBins, auto = False, plot = Fals
     if plot:
         plt.plot(bin_edges[:len(PAS)], PAS)
         plt.show()
-def density_scatter( x , y, ax = None, xlow = 0, xhigh = 16000, ylow = 1, yhigh = 1.5, sort = True, bins = 20, xlabel = 'x', ylabel = 'y', limFlag = True, **kwargs )   :
-    """
-    Scatter plot colored by 2d histogram
-    """
-    if ax is None :
-        fig , ax = plt.subplots()
-    data , x_e, y_e = np.histogram2d( x, y, bins = bins, density = True )
-    z = interpn( ( 0.5*(x_e[1:] + x_e[:-1]) , 0.5*(y_e[1:]+y_e[:-1]) ) , data , np.vstack([x,y]).T , method = "splinef2d", bounds_error = False)
-
-    #To be sure to plot all data
-    z[np.where(np.isnan(z))] = 0.0
-
-    # Sort the points by density, so that the densest points are plotted last
-    if sort :
-        idx = z.argsort()
-        x, y, z = x[idx], y[idx], z[idx]
-    if limFlag:
-        plt.ylim(ylow, yhigh)
-        plt.xlim(xlow, xhigh)
-    ax.scatter( x, y, c=z, s=0.05,cmap = 'rainbow', **kwargs )
-    norm = Normalize(vmin = np.min(z), vmax = np.max(z))
-    cbar = fig.colorbar(cm.ScalarMappable(norm = norm), ax=ax)
-    cbar.ax.set_ylabel('Density')
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    plt.show()
-    return ax
 def readExport (filepath):
     with open(filepath) as f:
         x = []
