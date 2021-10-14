989ca04b33ed046cda36cecd64af03ab46ac4e71
Khai <khai.ngo@stfc.ac.uk>
2021-10-14 09:14:16 +0100

    Fix bug the would dig up .dat in subdirs

--- Scintillator-multi-file.sh
+++ Scintillator-multi-file.sh
@@ -1,4 +1,4 @@
-for datafile in $(find $2 -name '*.dat' | sort -n -t _ -k 2)
+for datafile in $(find $2 -maxdepth 1 -name '*.dat' | sort -n -t _ -k 2)
 do
 	py Scintillator-analysis.py $1 $datafile $3
 done
