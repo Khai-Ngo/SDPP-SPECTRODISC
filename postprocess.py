8c2f7c43358485fc2a6d5d80b36a4cc63335d142
Khai <khai.ngp@'stfc.ac.uk>
2021-12-07 10:44:45 +0000

    Improve code aesthetics

--- postprocess.py
+++ postprocess.py
@@ -3,19 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.colors import Normalize 
 from scipy.interpolate import interpn
-import sys
 def readExport (filepath, max_rows = None):
     x, y = np.genfromtxt(filepath, dtype = (float, float), delimiter = '\t', missing_values = 'nan', usecols = (0,1), max_rows = max_rows, unpack = True)
     return x, y
-def dataTruncator (x, y, xlow = 0, xhigh = np.inf, ylow = 0, yhigh = np.inf, numPoint = np.inf):
+def dataTruncator (x, y, xlow = -np.inf, xhigh = np.inf, ylow = -np.inf, yhigh = np.inf, numPoint = np.inf):
     xret = []
     yret = []
     count = 0
     assert len(x) == len(y), "The arrays are not of the same length"
-    for i in range (len(x)):
-        if x[i] < xhigh and x[i] >= xlow and y[i] < yhigh and y[i] >= ylow:
-            xret.append(x[i])
-            yret.append(y[i])
+    for xelem, yelem in zip(x, y):
+        if xlow<=xelem<xhigh and ylow<=yelem<yhigh:
+            xret.append(xelem)
+            yret.append(yelem)
             count+=1
         if count > numPoint:
             break
@@ -23,42 +22,36 @@ def dataTruncator (x, y, xlow = 0, xhigh = np.inf, ylow = 0, yhigh = np.inf, num
     yret = np.array(yret)
     return xret, yret
 def buildHist(arr, filename, minVal, maxVal, noOfBins, auto = False, plot = False, save = True):
-    if auto:
-        PAS, bin_edges = np.histogram(arr, bins = 'auto') # auto-binning is used
-    else:
-        PAS, bin_edges = np.histogram(arr, bins = noOfBins, range = (minVal, maxVal))
+    hist, bin_edges = np.histogram(arr, bins = noOfBins, range = (minVal, maxVal)) if not auto else np.histogram(arr, bins = 'auto')
     if save:
-        f = open(filename,"w")
-        f.write('Value\tCounts\n')
-        for i in range(len(PAS)):
-            f.write('%s\t%s\n'%(bin_edges[i], PAS[i]))
-        f.close()
+        with open(filename,"w") as f:
+            f.write('Value\tCounts\n')
+            for e, val in zip(bin_edges[:-1], hist):
+                f.write('%s\t%s\n'%(e, val))
     if plot:
-        plt.plot(bin_edges[:len(PAS)], PAS)
+        plt.plot(bin_edges[:-1], hist)
         plt.show()
-def density_scatter( x , y, ax = None, xlow = 0, xhigh = 16000, ylow = 1, yhigh = 1.5, sort = True, bins = 20, xlabel = 'x', ylabel = 'y', limFlag = True)   :
+def density_scatter( x , y, xlow, xhigh, ylow, yhigh, ax = None, sort = True, bins = 20, xlabel = 'x', ylabel = 'y', fontsize = 18)   :
     """
     Scatter plot colored by 2d histogram
     """
-    fontsize = 18
     if ax is None :
         fig , ax = plt.subplots()
     data , x_e, y_e = np.histogram2d( x, y, bins = bins, density = True )
     z = interpn( ( 0.5*(x_e[1:] + x_e[:-1]) , 0.5*(y_e[1:]+y_e[:-1]) ) , data , np.vstack([x,y]).T , method = "splinef2d", bounds_error = False)
-
     #To be sure to plot all data
     z[np.where(np.isnan(z))] = 0.0
-
     # Sort the points by density, so that the densest points are plotted last
     if sort :
         idx = z.argsort()
         x, y, z = x[idx], y[idx], z[idx]
-    if limFlag:
-        plt.ylim(ylow, yhigh)
-        plt.xlim(xlow, xhigh)
+    # configure plot aesthetics
+    plt.xlim(xlow, xhigh)
+    plt.ylim(ylow, yhigh)
     plt.rcParams['font.size'] = fontsize
     for label in (ax.get_xticklabels()+ax.get_yticklabels()):
         label.set_fontsize(fontsize)
+    # actual plotting
     ax.scatter( x, y, c=z, s=0.05,cmap = 'rainbow')
     norm = Normalize(vmin = np.min(z), vmax = np.max(z))
     cbar = fig.colorbar(cm.ScalarMappable(norm = norm), ax=ax)
