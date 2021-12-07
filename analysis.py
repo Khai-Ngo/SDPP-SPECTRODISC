117a00f0d985573021155f9cb8329df91392aba9
Khai <khai.ngo@stfc.ac.uk>
2021-12-07 15:46:10 +0000

    CAEN method analyses data from CAEN wavedump.exe

--- analysis.py
+++ analysis.py
@@ -1,7 +1,12 @@
 import struct
 import scipy.integrate
-import sys
+import os
+import re
 
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(data, key=alphanum_key)
 def pulseAreaCLYC (pulse, threshold, pregate, allGate, W1 = 80, W2 = 500, delay = 20):
     """
     PSD method usually applied to CLYC
@@ -41,15 +46,24 @@ def pulseAreaEJ276 (pulse, threshold, pregate, shortGate =60, longGate = 220):
     stopShort = startIndex + shortGate
     stopLong = startIndex + longGate if startIndex + longGate < len(pulse) else len(pulse) -1
     # perform integration
-    shortInte = scipy.integrate.trapz (pulseArray[startIndex:stopShort+1], None, dx = 1.0) 
-    longInte = scipy.integrate.trapz (pulseArray[startIndex:stopLong+1], None, dx = 1.0)
+    shortInte = scipy.integrate.trapz (pulse[startIndex:stopShort+1], None, dx = 1.0) 
+    longInte = scipy.integrate.trapz (pulse[startIndex:stopLong+1], None, dx = 1.0)
     PSD = longInte/shortInte
     return longInte, PSD
 def save(x, y, z, outPath):
+    """
+    Write data to text file. Current suppports writing 3 lists
+    But could extend to writing any number of lists
+    Clears all lists from memory after writing to file
+    """
     assert len(x) == len(y) and len(x) == len(z), "To write to txt, need same length array!"
     with open(outPath, "a") as f:
         for x_, y_, z_, in zip(x, y, z):
             f.write("{}\t{}\t{}\n".format(x_, y_, z_))
+    # clear lists after writing to file
+    x.clear()
+    y.clear()
+    z.clear()
 def trapezoidal (pulse, L = 2, G = 7):
     pulse_Ftrap = []
     for i in range (len(pulse)-G-2*L):
@@ -72,3 +86,55 @@ def pile_up (pulse, step = 5, cfd = 0.15):
         if count > 2:
             return True
     return False
+def CAEN(fname, output, mode, threshold, pregate, **kwargs):
+    # initialise variables
+    areaArray, ratioArray, tArr = [], [], []
+    pulsenum = 0 
+    # select correct analysis method
+    if mode == 1:
+        analyse = pulseAreaEJ276
+    elif mode == 2:
+        analyse = pulseAreaCLYC
+    else:
+        raise ValueError('Invalid mode. Valid modes are 1 or 2')
+    # fname coukd either be a file or a folder
+    fileList = [fname + "\\" + name for name in sorted_alphanumeric(os.listdir(fname))] if os.path.isdir(fname) else [fname]   
+    for a_file in fileList:
+        if a_file.endswith(".dat"):
+            with open(a_file, 'rb') as f:
+                print("Reading from {}".format(a_file))
+                while True:
+                    # read header for pulse length and time stamp
+                    buffer = f.read(24)
+                    if not buffer: break # eof exit
+                    length, idk2, idk3, idk4, idk5, this_time_stamp = struct.unpack('=iiiiiI', buffer)
+                    length = int((length-24)/2)
+                    currentTimeStamp = float(this_time_stamp) * 0.008 # in microseconds
+                    # read and analyse pulse
+                    pulse = struct.unpack(f'={length}h', f.read(2*length))
+                    pulsenum += 1
+                    baseline = float(sum(pulse[:10]))/10
+                    pulse = [baseline - float(elem) for elem in pulse] # subtract baseline from pulse
+                    # recording counts
+                    if max(pulse) > threshold:
+                        PH, r = analyse(pulse, threshold, pregate, **kwargs)
+                        areaArray.append(PH)
+                        ratioArray.append(r)
+                        tArr.append(currentTimeStamp)
+                    if pulsenum % 10000 == 0: # save to file every batch of 10k pulses
+                        print ("Analysed {} pulses".format(pulsenum))
+                        print("Total number of accepted pulse: {}".format(len(areaArray)))
+                        save(areaArray, ratioArray, tArr, output)
+            # dat file has closed
+            print("Analysed {} pulses".format(pulsenum))
+            print("Total number of accepted pulse: {}\n".format(len(areaArray)))
+            save(areaArray, ratioArray, tArr, output) # save leftovers
+    print("Analysis complete!\n")
+def Pico(fname, outpupt, mode, threshold, pregate, **kwargs):
+    return
+def main():
+    # for module testing
+    import sys
+    CAEN(sys.argv[1], sys.argv[2], mode = 2, threshold = 10, pregate = 8, allGate = 1800, W1 = 80, W2 = 500, delay = 20) 
+if __name__ == '__main__':
+    main()
