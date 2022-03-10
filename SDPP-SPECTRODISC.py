0145456a76a01cd8c457d2ce3dc8302bd7d101ea
Khai <khai.ngo@stfc.ac.uk>
2022-03-10 09:48:00 +0000

    Remove max limit on the number of points on scatterplot

--- SDPP-SPECTRODISC.py
+++ SDPP-SPECTRODISC.py
@@ -77,7 +77,7 @@ class dropdownMenus:
         return self.clicked.get()
 def plotScatterplot():
     global truncate
-    x, y = pp.readExport(inFile2.get(), max_rows = 1000000)
+    x, y = pp.readExport(inFile2.get())
     if a_const.get() and b_const.get():
         a = float(a_const.get())
         b = float(b_const.get())
